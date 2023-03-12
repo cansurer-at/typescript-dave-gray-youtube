c15c4e481873b853f82838064ea87a2b2668365f
Can Surer <cansurer@outlook.com>
2023-03-12 21:00:33 +0100

    lesson 2

---
+++ lesson02/build/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+let myName = "Dave";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "John";
+meaningOfLife = 42;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ lesson02/src/main.ts
@@ -0,0 +1,17 @@
+let myName = "Dave";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+
+myName = "John"
+meaningOfLife = 42;
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+let postId: string | number 
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
