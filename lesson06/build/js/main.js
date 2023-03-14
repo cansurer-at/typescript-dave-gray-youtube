1e7c350c038bff8ce067e9d386d6f98426d6ec89
Can Surer <cansurer@outlook.com>
2023-03-14 17:25:01 +0100

    classes start

---
+++ lesson06/build/js/main.js
@@ -0,0 +1,20 @@
+"use strict";
+// Type Assertions(asorsin) & Type Casting (same thing has 2 name)
+// convert to more or less specific
+let a = "hello";
+let b = a; // less specific
+let c = a; // more spe
+let d = "word";
+let e = "word";
+const addOrConcat = (a, b, c) => {
+    if (c === "add")
+        return a + b;
+    return "" + a + b;
+};
+let myVal = addOrConcat(2, 2, "concat");
+// be careful! TS sees no problem- but string is returned
+let nextVal = addOrConcat(2, 2, "concat");
+// the DOM 
+const img = document.getElementById("img");
+const myImg = document.getElementById("#img");
+const nextImg = document.getElementById("#img");
